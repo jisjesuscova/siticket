b184b3d4dae40d1aec9a82a725108b2fd9111832
jisjesuscova <jesuscova@jisparking.com>
2023-04-29 18:04:45 -0400

    Initial

---
+++ resources/js/app.js
@@ -0,0 +1,15 @@
+import './bootstrap';
+
+import './jquery/jquery.min.js';
+
+import './bootstrap/js/bootstrap.bundle.min.js';
+
+import './js/adminlte.js';
+
+import './js/pages/dashboard3.js';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
